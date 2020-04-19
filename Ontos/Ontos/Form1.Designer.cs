a00e56ad071cff55c4a37631e87d7c56de660be8
AbandonedGaming <coonmatt3@gmail.com>
2020-04-19 15:19:29 -0400

    Started Concept Layout
    Started making a concept of the window layout and coded a few functions so it's not all just dummy buttons.

--- Ontos/Ontos/Form1.Designer.cs
+++ Ontos/Ontos/Form1.Designer.cs
@@ -29,22 +29,129 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resourcesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theXenoSeriesWikiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.informationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resourcesToolStripMenuItem,
+            this.informationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resourcesToolStripMenuItem
+            // 
+            this.resourcesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.theXenoSeriesWikiToolStripMenuItem});
+            this.resourcesToolStripMenuItem.Name = "resourcesToolStripMenuItem";
+            this.resourcesToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.resourcesToolStripMenuItem.Text = "Resources";
+            // 
+            // theXenoSeriesWikiToolStripMenuItem
+            // 
+            this.theXenoSeriesWikiToolStripMenuItem.Name = "theXenoSeriesWikiToolStripMenuItem";
+            this.theXenoSeriesWikiToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.theXenoSeriesWikiToolStripMenuItem.Text = "The Xeno Series Wiki";
+            this.theXenoSeriesWikiToolStripMenuItem.Click += new System.EventHandler(this.TheXenoSeriesWikiToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chapter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Chapter is defined by system saves";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(65, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Quests in selected chapter:";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(15, 98);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(169, 334);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // informationToolStripMenuItem
+            // 
+            this.informationToolStripMenuItem.Name = "informationToolStripMenuItem";
+            this.informationToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.informationToolStripMenuItem.Text = "Information";
+            this.informationToolStripMenuItem.Click += new System.EventHandler(this.InformationToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ontos - A Xenoblade Chronicles Guide";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resourcesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theXenoSeriesWikiToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ToolStripMenuItem informationToolStripMenuItem;
     }
 }
 
--- Ontos/Ontos/Form1.cs
+++ Ontos/Ontos/Form1.cs
@@ -16,5 +16,76 @@ namespace Ontos
         {
             InitializeComponent();
         }
+
+        //Creates a second custom form window
+        Form2 secondForm = new Form2();
+
+
+
+        //Error Message Template
+        //How To Use:
+        //In area the triggers error, list the following code:
+        //
+        //  string errormessage = "<INSERT ERROR MESSAGE>";
+        //  ProgramFailure();
+        //
+        //The window will display the error message set by the error
+        //in the custom error message box.
+        private void ProgramFailure()
+        {
+            //Template for Error Messages
+            string title = "Ontos - Error";
+            MessageBox.Show(errormessage, title);
+        }
+
+
+
+
+        //Xeno Wiki Reference Menu Code
+        //This just opens the link to the Xeno Series Wiki
+        private void TheXenoSeriesWikiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Open the link to the Xeno Series Wiki in the default web browser
+            //If link fails to open, display an error message.
+            try
+            {
+                VisitXenoWiki();
+            }
+            catch (Exception ex)
+            {
+                string errormessage = "Failed to open link.";
+                ProgramFailure();
+            }
+        }
+
+        //Opens the Xeno Series Wiki
+        private void VisitXenoWiki()
+        {
+            //Calls the Process.Start method to open the default browser
+            //Opens a link to the Xeno Series Wiki
+            System.Diagnostics.Process.Start("http://www.xenoserieswiki.org/wiki/Main_Page");
+        }
+
+
+
+
+
+        //IGNORE THIS CODE BLOCK
+        private void Label3_Click(object sender, EventArgs e)
+        {
+            //IGNORE THIS CODE BLOCK, THIS IS IF YOU CLICK THE TITLE
+            //ABOVE THE QUEST SELECT BOX
+        }
+
+
+
+
+        //The Information Menu Item Code
+        private void InformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Meh - Placeholder Text";
+            string title = "Ontos - Information";
+            MessageBox.Show(message, title);
+        }
     }
 }
